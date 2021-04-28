8b325bab7ee75daebd29c17350b1a2404d79aaa1
Meghan McFarland <meghanmc04@mac.com>
2021-04-28 14:51:16 -0400

    feat(server) adds morgan logging middleware to server

--- server/index.js
+++ server/index.js
@@ -1,15 +1,18 @@
 const express = require("express");
 const path = require("path");
-const PORT = process.env.PORT || 8080;
+const morgan = require("morgan");
 
+const PORT = process.env.PORT || 8080;
 const app = express();
 
+app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "../public")));
-app.get("/api", (req, res) => {
-  res.json({ message: "HAI I'M PAUL" });
-});
+
+// app.get("/api", (req, res) => {
+//   res.json({ message: "HAI I'M PAUL" });
+// });
 
 app.listen(PORT, () => {
   console.log(`tell me something good, ${PORT}`);
