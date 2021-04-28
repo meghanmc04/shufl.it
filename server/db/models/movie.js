60f0fd5b57514154e6b856b6322b04a20dde3d28
Meghan McFarland <meghanmc04@mac.com>
2021-04-28 15:28:31 -0400

    feat(db/models) creates movie model

---
+++ server/db/models/movie.js
@@ -0,0 +1,33 @@
+const Sequelize = require("sequelize");
+const db = require("../db");
+
+Movie = db.define("movie", {
+  title: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true,
+    },
+  },
+  platform: {
+    type: Sequelize.STRING,
+  },
+  image: {
+    type: Sequelize.STRING,
+  },
+  description: {
+    type: Sequelize.TEXT,
+  },
+  year: {
+    type: Sequelize.INTEGER,
+  },
+  rating: {
+    type: Sequelize.STRING,
+  },
+  seen: {
+    type: Sequelize.BOOLEAN,
+    defaultValue: false,
+  },
+});
+
+module.exports = Movie;
